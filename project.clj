5eb504561e4cb4410af6e86daea5c050c9d6b9fd
Mark Whelan <m@townunderground.net>
2014-12-28 14:59:49 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject sunrise "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject sunrise "0.1.0"
+  :description "Get sunrise and sunset times, given a location and date."
+  :url "https://github.com/mrwhelan/sunrise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
