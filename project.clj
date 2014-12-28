86fe363a6ff79f51416a1b4972798323b5a52a51
Mark Whelan <m@townunderground.net>
2014-12-28 15:30:36 -0500

    add namespace to project.clj, and reset clj version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject sunrise "0.1.0"
+(defproject net.townunderground/sunrise "0.1.0"
   :description "Get sunrise and sunset times, given a location and date."
   :url "https://github.com/mrwhelan/sunrise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
